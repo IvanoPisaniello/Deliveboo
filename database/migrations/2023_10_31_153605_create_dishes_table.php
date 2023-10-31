59f62ffafb94ab79c0f3f95ddd9667ad0d20d96f
GabrielePazi <pazigabriele@gmail.com>
2023-10-31 17:12:56 +0100

    create dish model and migration

---
+++ database/migrations/2023_10_31_153605_create_dishes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->decimal('price', 5, 2);
+            $table->boolean('visible');
+            $table->string('image');
+            $table->tinyInteger('discount');
+            $table->text('ingredients');
+
+            // $table->unsignedBigInteger('resturant_id');
+            // $table->foreign('resturant_id')->references('id')->on('resturants');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        // Schema::dropForeign('dishes_resturant_id');
+        Schema::dropIfExists('dishes');
+    }
+};
