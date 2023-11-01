383e83fef6dadaeb247c437d0b1c2a8aa7875798
ElisaIanda <elisaianda01@gmail.com>
2023-11-01 12:31:40 +0100

    Add file crud

--- resources/views/admin/dish/index.blade.php
+++ resources/views/admin/dish/index.blade.php
@@ -1,5 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-    
+    <div class="container">
+        <div class="row">
+            <div>
+                <a href="{{ route('admin.dishes.create') }}" class="btn btn-outline-primary my-4 ">Aggiungi Piatto</a>
+            </div>
+
+            @foreach ($dishes as $singleDish)
+                <div class="col-4 mb-4">
+                    <div class="card">
+
+                        <img src="{{ $singleDish->image }}">
+                        {{-- <img src="{{ asset('/storage/' . $singleDish['image']) }}" alt="{{ $singleDish->title }}"
+                    style="height: 200px"> --}}
+
+                        <div class="card-body">
+                            <h3 class="text-center">{{ $singleDish->title }}</h3>
+
+                            {{-- <div class="">{{ $singleDish->type?->title }}</div> --}}
+
+                            <div class="text-center">
+                                <a href="{{ route('admin.dishes.show', $singleDish->id) }}" class="btn btn-primary small"
+                                    type="button">Dettagli
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
--- resources/views/admin/dish/show.blade.php
+++ resources/views/admin/dish/show.blade.php
@@ -1,5 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
-    
+<div class="container">
+    <div class="row">
+        <div>
+            <a href={{ route('admin.dishes.index', ['id' => $dish->id]) }}
+                class="btn btn-outline-primary my-4 ">Indietro</a>
+            <a href={{ route('admin.dishes.edit', $dish->id) }} class="btn btn-outline-warning my-4">Modifica
+            </a>
+
+            <form action="{{ route('admin.dishes.destroy', $dish->id) }}" method="POST" class="d-inline-block">
+                @csrf
+
+                {{-- Passo il vero metodo che voglio usare --}}
+                @method('DELETE')
+                <input type="submit" value="Elimina" class="btn btn-outline-danger">
+            </form>
+        </div>
+
+        <div class="card my-3 p-0">
+            <img src="{{ $dish->image }}">
+
+            {{-- <img src="{{ asset('/storage/' . $project->image) }}" class="card-img-top w-100"
+                alt="{{ $project->title }}">
+            <div class="card-body"> --}}
+                <h5 class="card-title">{{ $dish->title }}</h5>
+
+                <p class="card-text">{{ $dish->description }}</p>
+                <p class="card-text">{{ $dish->ingredients }}</p>
+
+
+                <div>
+                    
+
+                <p class="card-text">
+                    <small class="text-body-secondary">
+                        <div class="card-text">{{ $dish->price }}</div>
+                    </small>
+                </p>
+
+            </div>
+        </div>
+
+    </div>
+</div>
 @endsection
