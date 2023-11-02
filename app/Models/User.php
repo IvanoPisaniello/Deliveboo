04b5f41f2b2973d6a01dada18495e4e3f2390afa
ElisaIanda <elisaianda01@gmail.com>
2023-11-02 16:43:08 +0100

    Add field in form register

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'restaurant_name',
+        'vat',
+        'user_address',
+        // 'type',
+
     ];
 
     /**
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,70 @@
                             </div>
                         </div>
 
+
+                        {{-- restaurant name --}}
+                        <div class="mb-4 row">
+                            <label for="restaurant_name" class="col-md-4 col-form-label text-md-right">{{ __('Restaurant Name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="restaurant_name" type="text" class="form-control @error('restaurant_name') is-invalid @enderror" name="restaurant_name" value="{{ old('restaurant_name') }}" required autocomplete="restaurant_name">
+
+                                @error('restaurant_name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        {{-- vat name --}}
+                        <div class="mb-4 row">
+                            <label for="vat" class="col-md-4 col-form-label text-md-right">{{ __('Vat') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="vat" type="text" class="form-control @error('vat') is-invalid @enderror" name="vat" value="{{ old('vat') }}" required autocomplete="vat">
+
+                                @error('vat')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+
+                        {{-- user address  --}}
+                        <div class="mb-4 row">
+                            <label for="user_address" class="col-md-4 col-form-label text-md-right">{{ __('User Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="user_address" type="text" class="form-control @error('user_address') is-invalid @enderror" name="user_address" value="{{ old('User Addres') }}" required autocomplete="user_address">
+
+                                @error('user_address')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+
+
+                        {{-- type_id --}}
+                        {{-- <div class="mb-4 row">
+                            <label for="type_id" class="col-md-4 col-form-label text-md-right">{{ __('type_id') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="type_id" type="text" class="form-control @error('type_id') is-invalid @enderror" name="type_id" value="{{ old('User Addres') }}" required autocomplete="type_id">
+
+                                @error('type_id')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div> --}}
+
                         <div class="mb-4 row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
