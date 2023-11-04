5a5167af9e22ae20099ef37f54f02295f1a434ca
GabrielePazi <pazigabriele@gmail.com>
2023-11-04 17:30:49 +0100

    adds filter for restaurants depending on types

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -6,14 +6,31 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\Models\Restaurant;
+use App\Models\Type;
 
 class RestaurantController extends Controller
 {
-    public function index() {
+    public function index()
+    {
+        $queryString = request()->query();
 
-        $restaurants = Restaurant::with(['user', 'types', 'dishes', 'dishes.category',
-        'dishes.orders', 'orders'])
-        ->get();
+        dd($queryString);
+
+        if (isset($queryString) && $queryString != '' && Type::where('name', $queryString)->exists()) {
+            $typeToSearch = Type::where('name', $queryString)->get()->first();
+
+            $relatedRestaurants = $typeToSearch->restaurants;
+
+            return response()->json([
+                'results' => $relatedRestaurants,
+                'count' => $relatedRestaurants->count()
+            ]);
+        }
+
+        $restaurants = Restaurant::with([
+            'user', 'types', 'dishes', 'dishes.category',
+            'dishes.orders', 'orders'
+        ])->get();
 
         return response()->json([
             'results' => $restaurants,
@@ -21,10 +38,11 @@ class RestaurantController extends Controller
         ]);
     }
 
-    public function show($slug) {
+    public function show($slug)
+    {
         $restaurant = Restaurant::where('slug', $slug)
-        ->with(['user', 'types', 'dishes', 'dishes.category', 'dishes.orders', 'orders'])
-        ->first();
+            ->with(['user', 'types', 'dishes', 'dishes.category', 'dishes.orders', 'orders'])
+            ->first();
 
         if (!$restaurant) {
             abort(404);
