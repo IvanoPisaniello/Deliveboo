f53b305886370ef87c45b43624a9ae3bdfab34a5
IvanoPisaniello <ivanop92@gmail.com>
2023-11-04 12:47:52 +0100

    added slug control

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -4,8 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Restaurant;
 use Illuminate\Http\Request;
+use App\Models\Dish;
+use Illuminate\Support\Str;
 
 class RestaurantController extends Controller
 {
-    //
 }
