cedd7425189333239cb33712c51573f4bb838ed2
IvanoPisaniello <ivanop92@gmail.com>
2023-11-07 18:13:02 +0100

    deleted span

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -60,7 +60,7 @@
                                 <div class="col-md-6">
                                     <input id="password" type="password"
                                         class="form-control @error('password') is-invalid @enderror" name="password"
-                                        required autocomplete="new-password"><span class="text-danger">*</span>
+                                        required autocomplete="new-password">
                                         
                                     @error('password')
                                         <span class="invalid-feedback" role="alert">
