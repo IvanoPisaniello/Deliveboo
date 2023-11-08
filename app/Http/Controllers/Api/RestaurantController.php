dabb38261431aa64b45103b31c81745a809214f3
GabrielePazi <pazigabriele@gmail.com>
2023-11-08 21:27:46 +0100

    fixa filtro tramite checkbox(spero)

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -16,32 +16,57 @@ class RestaurantController extends Controller
 
         //se la querystring è settata ed è diversa da '' allora parte il filtro
         if (isset($query) && $query != []) {
-            $queryString = explode(',', $query['type']);
+            // FILTRO DELLE CARD DEI RISTORANTI:
+            // if ($query['type']) {
+            //     dump('la query è type');
+            //     $queryString = explode(',', $query['type']);
 
-            if (Type::where('name', $queryString)->exists()) {
-                //FILTRO SBAGLIATO TENUTO PER RICORDO :)
-                // $typeToSearch = [];
+            //     if (Type::where('name', $queryString)->exists()) {
+            //         $typeToSearch = Type::where('name', $queryString)->get()->first();
 
-                // for ($i=0; $i < count($queryString); $i++) { 
-                //     array_push($typeToSearch, Type::where('name', $queryString[$i])->get()->first());
-                // }
-                // dump($typeToSearch);
+            //         $relatedRestaurants = $typeToSearch->restaurants;
+            //     }
+            //     return response()->json([
+            //         'results' => $relatedRestaurants,
+            //         'count' => $relatedRestaurants->count(),
+            //     ]);
+            // }
 
-                // for ($i=0; $i < count($typeToSearch); $i++) { 
-                //     if ($typeToSearch[$i]->restaurants) {
-                //         $relatedRestaurants = $typeToSearch[$i]->restaurants;
-                //     }
-                // }
-                //$relatedRestaurants = $typeToSearch->restaurants;
+            //FILTRO DEI RISTORANTI TRAMITE CHECKBOX
+            $selectedTypes = [];
+            foreach ($query as $key => $el) {
+                if ($el['value'] == 'true') {
+                    $type = Type::where('name', $key)->get()->first();
+                    array_push($selectedTypes, $type['id']);
+                }
+            }
 
-                $typeToSearch = Type::where('name', $queryString)->get()->first();
+            //$selectedTypes = [1, 2]; Esempio: tipi selezionati con i loro ID (1 per pizzeria, 2 per sushi)
+            $relatedRestaurants = Restaurant::whereHas('types', function ($query) use ($selectedTypes) {
+                $query->whereIn('type_id', $selectedTypes);
+            }, '>=', count($selectedTypes))->get();
 
-                $relatedRestaurants = $typeToSearch->restaurants;
+            //recupero dei type dei ristoranti filtrati
+            for ($i = 0; $i < count($relatedRestaurants); $i++) {
+                $relatedRestaurants[$i] = Restaurant::where('name', $relatedRestaurants[$i]['name'])->with(['types'])->get()[0];
+            }
+
+            //SE NON TROVA RISTORANTI LI RESTITUISCE TUTTI CON UN MESSAGGIO DI ERRORE IN ALLEGATO
+            if (count($relatedRestaurants) == 0) {
+                $restaurants = Restaurant::with([
+                    'user', 'types', 'dishes', 'dishes.category',
+                    'dishes.orders', 'orders'
+                ])->get();
+
+                return response()->json([
+                    'results' => $restaurants,
+                    'count' => $restaurants->count(),
+                    'error' => 'Nessuna corrispondenza'
+                ]);
             }
 
             return response()->json([
-                'results' => $relatedRestaurants,
-                'count' => $relatedRestaurants->count(),
+                'results' => $relatedRestaurants
             ]);
         }
 
@@ -69,3 +94,20 @@ class RestaurantController extends Controller
         return response()->json($restaurant);
     }
 }
+
+
+
+//FILTRO SBAGLIATO TENUTO PER RICORDO :)
+// $typeToSearch = [];
+
+// for ($i=0; $i < count($queryString); $i++) { 
+//     array_push($typeToSearch, Type::where('name', $queryString[$i])->get()->first());
+// }
+// dump($typeToSearch);
+
+// for ($i=0; $i < count($typeToSearch); $i++) { 
+//     if ($typeToSearch[$i]->restaurants) {
+//         $relatedRestaurants = $typeToSearch[$i]->restaurants;
+//     }
+// }
+//$relatedRestaurants = $typeToSearch->restaurants;
