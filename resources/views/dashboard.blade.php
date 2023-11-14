211bbb50eb1e98bff540cfdfdb93e3164956f2b5
MatteoArt <mattrim8@gmail.com>
2023-11-14 22:41:08 +0100

    added chart orders

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container-fluid h-100">
-    <h2 class="fs-4 text-secondary my-4">
+    <h2 class="fs-4 text-secondary my-4 p-4">
         Benvenuto nella tua DASHBOARD: {{ Auth::user()->name }}
     </h2>
 
@@ -19,8 +19,11 @@
             <img src="{{ asset('storage/' . Auth::user()->restaurant->image) }}" alt="Restaurant Image" class="img-fluid rounded-4" style="height: 100%; object-fit: cover;">
         </div>
     </div>
-    <div class="p-4 mt-3">
-        <h3><a href="{{ route('dashboard.orders') }}" class="link-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">
+    <div class="d-flex p-4 mt-3">
+        <h3 class="pe-5"><a href="{{ route('dashboard.orders') }}" class="link-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">
+            Vedi riepilogo ordini
+        </a></h3>
+        <h3><a href="{{ route('dashboard.chart') }}" class="link-success link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">
             Vedi statistiche ordini
         </a></h3>
     </div>
