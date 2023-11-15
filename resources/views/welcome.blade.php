bc3709481d3921be2c93aeac84095b2ff8532b20
ElisaIanda <elisaianda01@gmail.com>
2023-11-15 12:17:40 +0100

    fix welcome-page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,24 +1,32 @@
 @extends('layouts.app')
 @section('content')
+    <div class="jumbotron p-5 mb-4 bg-light rounded-3">
+        <div class="container">
+            <div class="row justify-content-center align-items-center g-4">
+                <div class="col-sm-12 col-lg-6">
+                    <div>
+                        <img src="{{ asset('img/logo_deliveboo.png') }}" alt="logo-deliveboo" class="img-fluid w-100">
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-6">
+                    <h1 class="display-5 fw-bold ">
+                        Benvenuto in Deliveboo
+                    </h1>
 
-<div class="jumbotron p-5 mb-4 bg-light rounded-3">
-    <div class="container py-5">
-        <div style="width: 300px">
-            <img src="{{ asset('img/logo_deliveboo.png') }}" alt="logo-deliveboo"
-            class="img-fluid">
+                    <p class="fs-4">Questo è il pannello di controllo dove puoi gestire a 360 gradi il tuo
+                        ristorante,
+                        controllare gli ordini, inserire i piatti e vedere le statistiche della tua attività.</p>
+                    <a href="http://localhost:5174/" target="_blank" class="btn btn-orange-text-white fw-bold"
+                        type="button">Vai
+                        sull'app di Deliveboo</a>
+                </div>
+            </div>
         </div>
-        <h1 class="display-5 fw-bold mt-5">
-            Benvenuto in Deliveboo
-        </h1>
-
-        <p class="col-md-8 fs-4">Questo è il pannello di controllo dove puoi gestire a 360 gradi il tuo ristorante, controllare gli ordini, inserire i piatti e vedere le statistiche della tua attività.</p>
-        <a href="http://localhost:5174/" target="_blank" class="btn btn-success btn-lg" type="button">Vai sull'app di Deliveboo</a>
     </div>
-</div>
 
-<div class="content">
-    <div class="container">
-        <p>Diventa un partner di Deliveboo e gestisci la tua attività.</p>
+    <div class="content">
+        <div class="container ">
+            <p>Diventa un partner di Deliveboo e gestisci la tua attività.</p>
+        </div>
     </div>
-</div>
 @endsection
